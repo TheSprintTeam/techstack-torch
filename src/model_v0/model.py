a3e2500bf45474cfedcdda2a56860236e7078ad7
wuda17 <dwxcan@gmail.com>
2023-07-30 22:49:12 -0400

    try smaller deploy

--- src/model_v0/model.py
+++ src/model_v0/model.py
@@ -1,13 +1,13 @@
-import torch
-from transformers import pipeline
+# import torch
+# from transformers import pipeline
 
-model = "bert-large-uncased"
-bertNLP = pipeline("feature-extraction", model=model)
+# model = "bert-large-uncased"
+# bertNLP = pipeline("feature-extraction", model=model)
 
-def generate_sentence_embeddings(sentence: str) -> torch.Tensor:
-    embeddings = bertNLP(sentence)
-    embeddings_list = [torch.tensor(embedding) for embedding in embeddings[0]]
-    embeddings_tensor = torch.stack(embeddings_list)
-    cls_index = 0
-    cls_embedding = embeddings_tensor[cls_index]
-    return cls_embedding
+# def generate_sentence_embeddings(sentence: str) -> torch.Tensor:
+#     embeddings = bertNLP(sentence)
+#     embeddings_list = [torch.tensor(embedding) for embedding in embeddings[0]]
+#     embeddings_tensor = torch.stack(embeddings_list)
+#     cls_index = 0
+#     cls_embedding = embeddings_tensor[cls_index]
+#     return cls_embedding
