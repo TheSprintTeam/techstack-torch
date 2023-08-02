27d447a233d7dfa60ec9aaaca848d4131c192084
wuda17 <dwxcan@gmail.com>
2023-08-02 12:37:12 -0400

    removed cache model and then added setup script to download BERT locally

--- src/model_v0/model.py
+++ src/model_v0/model.py
@@ -3,11 +3,22 @@ Module to run model for feature extraction.
 """
 
 import torch
-from transformers import pipeline
+from transformers import pipeline, AutoModel
 
 
-CACHED_DIRECTORY = "/app/cached_models/pretrained_bert"
-BERT_FEATURE_MODEL = pipeline("feature-extraction", model=CACHED_DIRECTORY)
+MODEL_NAME = "bert-large-uncased"
+CACHED_DIRECTORY = "/Users/Omega/dev/sprint/Recommendation-Engine/src/cached_models/pretrained_bert"#"/app/cached_models/pretrained_bert"
+
+
+if __name__ == "__main__":
+    """
+    Setup script to download the BERT model to memory in src/cached_models
+    """
+    model = AutoModel.from_pretrained(MODEL_NAME)
+    model.save_pretrained(CACHED_DIRECTORY)
+else:
+    BERT_FEATURE_MODEL = pipeline("feature-extraction", model=CACHED_DIRECTORY)
+
 
 def generate_sentence_embeddings(sentence: str) -> torch.Tensor:
     embeddings = BERT_FEATURE_MODEL(sentence)
